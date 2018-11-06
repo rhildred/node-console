f39c8516e72567a71390e8474589e079a3cbb465
Rich Hildred <rhildred@ip-172-31-5-173.us-west-2.compute.internal>
2018-11-06 11:51:15 -0500

    made it size to the window. Still not catching the resize event

--- client.ts
+++ client.ts
@@ -39,35 +39,8 @@ let socket;
 let pid;
 
 const terminalContainer = document.getElementById('terminal-container');
-const actionElements = {
-  findNext: <HTMLInputElement>document.querySelector('#find-next'),
-  findPrevious: <HTMLInputElement>document.querySelector('#find-previous')
-};
-const paddingElement = <HTMLInputElement>document.getElementById('padding');
-
-function setPadding(): void {
-  term.element.style.padding = parseInt(paddingElement.value, 10).toString() + 'px';
-  term.fit();
-}
 
 createTerminal();
-
-const disposeRecreateButtonHandler = () => {
-  // If the terminal exists dispose of it, otherwise recreate it
-  if (term) {
-    term.dispose();
-    term = null;
-    window.term = null;
-    socket = null;
-    document.getElementById('dispose').innerHTML = 'Recreate Terminal';
-  }
-  else {
-    createTerminal();
-    document.getElementById('dispose').innerHTML = 'Dispose terminal';
-  }
-};
-
-document.getElementById('dispose').addEventListener('click', disposeRecreateButtonHandler);
 
 function createTerminal(): void {
   // Clean terminal
@@ -92,41 +65,15 @@ function createTerminal(): void {
   term.open(terminalContainer);
   term.winptyCompatInit();
   term.webLinksInit();
+  console.log("width = " + terminalContainer.style.width);
+  console.log("height = " + terminalContainer.style.height);
   term.fit();
   term.focus();
 
-  addDomListener(paddingElement, 'change', setPadding);
-
-  addDomListener(actionElements.findNext, 'keypress', (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      const searchOptions = {
-        regex: (document.getElementById('regex') as HTMLInputElement).checked,
-        wholeWord: (document.getElementById('whole-word') as HTMLInputElement).checked,
-        caseSensitive: (document.getElementById('case-sensitive') as HTMLInputElement).checked
-      };
-      term.findNext(actionElements.findNext.value, searchOptions);
-    }
-  });
-  addDomListener(actionElements.findPrevious, 'keypress', (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      const searchOptions = {
-        regex: (document.getElementById('regex') as HTMLInputElement).checked,
-        wholeWord: (document.getElementById('whole-word') as HTMLInputElement).checked,
-        caseSensitive: (document.getElementById('case-sensitive') as HTMLInputElement).checked
-      };
-      term.findPrevious(actionElements.findPrevious.value, searchOptions);
-    }
-  });
 
   // fit is called within a setTimeout, cols and rows need this.
   setTimeout(() => {
     initOptions(term);
-    // TODO: Clean this up, opt-cols/rows doesn't exist anymore
-    (<HTMLInputElement>document.getElementById(`opt-cols`)).value = term.cols;
-    (<HTMLInputElement>document.getElementById(`opt-rows`)).value = term.rows;
-    paddingElement.value = '0';
 
     // Set terminal size again to set the specific dimensions on the demo
     updateTerminalSize();
@@ -228,67 +175,13 @@ function initOptions(term: TerminalType): void {
     }
   });
 
-  let html = '';
-  html += '<div class="option-group">';
-  booleanOptions.forEach(o => {
-    html += `<div class="option"><label><input id="opt-${o}" type="checkbox" ${term.getOption(o) ? 'checked' : ''}/> ${o}</label></div>`;
-  });
-  html += '</div><div class="option-group">';
-  numberOptions.forEach(o => {
-    html += `<div class="option"><label>${o} <input id="opt-${o}" type="number" value="${term.getOption(o)}"/></label></div>`;
-  });
-  html += '</div><div class="option-group">';
-  Object.keys(stringOptions).forEach(o => {
-    if (stringOptions[o]) {
-      html += `<div class="option"><label>${o} <select id="opt-${o}">${stringOptions[o].map(v => `<option ${term.getOption(o) === v ? 'selected' : ''}>${v}</option>`).join('')}</select></label></div>`;
-    } else {
-      html += `<div class="option"><label>${o} <input id="opt-${o}" type="text" value="${term.getOption(o)}"/></label></div>`;
-    }
-  });
-  html += '</div>';
 
-  const container = document.getElementById('options-container');
-  container.innerHTML = html;
-
-  // Attach listeners
-  booleanOptions.forEach(o => {
-    const input = <HTMLInputElement>document.getElementById(`opt-${o}`);
-    addDomListener(input, 'change', () => {
-      console.log('change', o, input.checked);
-      term.setOption(o, input.checked);
-    });
-  });
-  numberOptions.forEach(o => {
-    const input = <HTMLInputElement>document.getElementById(`opt-${o}`);
-    addDomListener(input, 'change', () => {
-      console.log('change', o, input.value);
-      if (o === 'cols' || o === 'rows') {
-        updateTerminalSize();
-      } else {
-        term.setOption(o, parseInt(input.value, 10));
-      }
-    });
-  });
-  Object.keys(stringOptions).forEach(o => {
-    const input = <HTMLInputElement>document.getElementById(`opt-${o}`);
-    addDomListener(input, 'change', () => {
-      console.log('change', o, input.value);
-      term.setOption(o, input.value);
-    });
-  });
 }
 
-function addDomListener(element: HTMLElement, type: string, handler: (...args: any[]) => any): void {
-  element.addEventListener(type, handler);
-  term._core.register({ dispose: () => element.removeEventListener(type, handler) });
-}
+terminalContainer.addEventListener("resize", updateTerminalSize);
 
 function updateTerminalSize(): void {
-  const cols = parseInt((<HTMLInputElement>document.getElementById(`opt-cols`)).value, 10);
-  const rows = parseInt((<HTMLInputElement>document.getElementById(`opt-rows`)).value, 10);
-  const width = (cols * term._core.renderer.dimensions.actualCellWidth + term._core.viewport.scrollBarWidth).toString() + 'px';
-  const height = (rows * term._core.renderer.dimensions.actualCellHeight).toString() + 'px';
-  terminalContainer.style.width = width;
-  terminalContainer.style.height = height;
+  terminalContainer.style.width = (terminalContainer.clientWidth - term._core.viewport.scrollBarWidth).toString()+"px";
+  terminalContainer.style.height = (terminalContainer.clientHeight - 20).toString()+"px";
   term.fit();
 }
